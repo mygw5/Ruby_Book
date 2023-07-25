4d476cba650b935b314880d56ffb97aa3a49d118
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-07-25 00:59:16 +0000

    [add] to_intsメソッド

--- lib/rgh.rb
+++ lib/rgh.rb
@@ -1,6 +1,7 @@
 def to_hex(r,g,b)
-  '#'+
-  r.to_s(16).rjust(2, '0') +
-  g.to_s(16).rjust(2, '0') +
-  b.to_s(16).rjust(2, '0')
+  [r, g, b].sum('#') do |n|
+    n.to_s(16).rjust(2, '0')
+  end
 end
+
+def to_ints(hex) =   hex.scan(/\w\w/).map(&:hex)
